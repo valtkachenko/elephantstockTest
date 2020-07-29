8a480b117676f4e0f9e7375efb95e3f4b5445ede
Valerii Tkachenko <valtkachenko.88@gmail.com>
2020-07-29 11:49:25 +0300

    feat: api url from environment variables

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../common.interfaces';
 import { TableFilter } from '../common.interfaces';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private baseUrl = 'http://localhost:3000/api/v1';
+  private baseUrl = environment.apiUrl;
 
   constructor(private readonly httpClient: HttpClient) { }
 
